ac66103c76c6baa1e5bd943b897d0ae63792ffb7
junaid1016 <ja2318558@gmail.com>
2026-02-11 07:09:59 +0530

    update Home page UI

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,20 +1,56 @@
 import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import "./header.css";
-import collegeLogo from "./college.png";
-import complaintLogo from "./sw.jpeg";
+
+import backIcon from "./back.png";
+import homeIcon from "./home.png";
 
 function Header() {
+  const navigate = useNavigate();
+  const location = useLocation();
 
+  const isHomePage = location.pathname === "/";
 
-    return (
-        <>
-            <div className="headerMain">
-                <img src={collegeLogo} alt="College Logo" className="logo leftLogo"/>
-                <p className="headerP">Student Complaint Management System</p>
-                <img src={complaintLogo} alt="Namme-Voice" className="logo rightLogo"/>
-            </div>
-        </>
-    );
+  return (
+    <div className="headerMain">
+
+      {/* 🔙 Back button – only on NON-home pages */}
+      {!isHomePage && (
+        <button className="navBtn backBtn" onClick={() => navigate(-1)}>
+          <img src={backIcon} alt="Back" className="navIcon" />
+        </button>
+      )}
+
+      {/* 🏷 Title */}
+      <div className="headerTitle">
+        Student Complaint Management System
+      </div>
+
+      {/* Right side buttons */}
+      <div className="headerRight">
+
+        {/* 🏠 HOME PAGE ONLY */}
+        {isHomePage && (
+          <>
+            <button className="navBtn textBtn" onClick={() => alert("About page")}>
+              About
+            </button>
+            <button className="navBtn textBtn" onClick={() => alert("Contact page")}>
+              Contact
+            </button>
+          </>
+        )}
+
+        {/* 📄 OTHER PAGES */}
+        {!isHomePage && (
+          <button className="navBtn homeBtn" onClick={() => navigate("/")}>
+            <img src={homeIcon} alt="Home" className="navIcon" />
+          </button>
+        )}
+
+      </div>
+    </div>
+  );
 }
 
 export default Header;
